3ac7ba9645d6b57bb4ca0e049c2d3d042a2f7547
TheAlejandRo <thealejandro7w7@gmail.com>
2019-07-04 16:01:42 -0600

    Correción de errores

--- Hospital Ultimate/My Project/Settings.Designer.vb
+++ Hospital Ultimate/My Project/Settings.Designer.vb
@@ -14,7 +14,7 @@ Option Explicit On
 
 
 <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
- Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.2.0.0"),  _
+ Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.1.0.0"),  _
  Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
 Partial Friend NotInheritable Class MySettings
     Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -125,7 +125,7 @@ Partial Friend NotInheritable Class MySettings
     
     <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-     Global.System.Configuration.DefaultSettingValueAttribute("server=192.168.1.2; user=""""; password=HospitalTurnos.app; database=dbturnos")>  _
+     Global.System.Configuration.DefaultSettingValueAttribute("server=192.168.0.9; user=""""; password=HospitalTurnos.app; database=dbturnos")>  _
     Public ReadOnly Property Server() As String
         Get
             Return CType(Me("Server"),String)
